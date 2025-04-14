809fef60d2a6068902cbd5ac0d0b723c927a1acb
hunngphan06 <phanvogiahung@gmail.com>
2025-04-14 18:22:33 +0700

    Initial commit: upload JSP & Servlet project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,8 @@ import java.io.File;
 import org.apache.catalina.Context;
 import org.apache.catalina.startup.Tomcat;
 
-public class Main {
+public class
+Main {
     public static void main(String[] args) {
         try {
 
